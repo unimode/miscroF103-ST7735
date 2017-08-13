7206e55610adfe333237bdadaafe4ebd1608d108
Roman A. Paramonov <paramra@mail.ru>
2017-08-13 09:38:32 +0300

    add new file

---
+++ Inc/ST7735.h
@@ -0,0 +1,15 @@
+/*
+ * lcdST7735.h
+ *
+ *  Created on: Aug 13, 2017
+ *      Author: paramra
+ */
+
+#ifndef __LCDST7735_H__
+#define __LCDST7735_H__
+
+void st7735Init(void);
+
+void st7735Test(void);
+
+#endif /* LCDST7735_H_ */
